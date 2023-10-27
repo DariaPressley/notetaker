0829200080eb98021b9b73d918f4648ec01b409f
DariaPressley <daria.mik@gmail.com>
2023-10-26 20:59:15 -0700

    completed serve js

--- Develop/server.js
+++ Develop/server.js
@@ -1,37 +1,49 @@
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+// const api = require('./server.js');
+const notes = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// // Import custom middleware, "cLog"
-// app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to get notess`);
+  return res.json(notes);
+});
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
-
-// Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, 'public/pages/index.html'))
 );
 
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  let response;
+
+
+  if (req.body && req.body.product) {
+    response = {
+      data: req.body,
+    };
+    res.json(`Review for ${response.data.product} has been added!`);
+  } else {
+    res.json('Request body must at least contain a product name');
+  }
+
+
+  console.log(req.body);
+});
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`Now listening at http://localhost:${PORT}`)
 );
+
